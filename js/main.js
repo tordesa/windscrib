ebe05560aa3998d5c4049bdfe45a74c2a8c7bc08
toor <odus@localhost.localdomain>
2021-05-22 10:43:21 +0100

    added an input field

--- js/main.js
+++ js/main.js
@@ -1,6 +1,61 @@
 console.log("WindScrib v1.0.1");
 
+const base = "https://api.openweathermap.org/data/2.5/";
+
 const API_KEY = "e33607657d15ab134399ef40a1c43892";
+
+const searchbox = document.querySelector('.search-box');
+
+const searchbtn = document.querySelector('.search-btn');
+
+searchbtn.addEventListener("click", setQuery);
+
+function setQuery(e) {
+  e.preventDefault();
+  getResults(searchbox.value)
+  console.log(searchbox.value);
+}
+
+async function getResults(query) {
+  // fetch(`${url}weather?q=${query}&units=metric&APPID=${API_KEY}`)
+  //   .then(weather => {
+  //     return weather.json();
+  //   }).then(displayResults);
+
+  const weatherResponse = await fetch(`${base}weather?q=${query}&units=metric&APPID=${API_KEY}`);
+
+  const weatherData = await weatherResponse.json();
+
+  console.log(weatherData)
+  displayResults(weatherData)
+  updateSearchReport(weatherData);
+}
+
+
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector(".location .city")
+  let latitude = document.querySelector(".location .latitude")
+  let longitude = document.querySelector(".location .longitude")
+  let humidity = document.querySelector(".location .humidity")
+  let pressure = document.querySelector(".location .pressure")
+  let speed = document.querySelector(".location .speed")
+  let status = document.querySelector(".location .status")
+  let temperature = document.querySelector(".location .temperature")
+  // let city = document.querySelector(".location .city")
+  // let city = document.querySelector(".location .city")
+  // let city = document.querySelector(".location .city")
+  city.innerText = `City:${weather.name}, ${weather.sys.country}`;
+  latitude.innerText = `Latitude: ${weather.coord.lat}`;
+  longitude.innerText = `Longitude: ${weather.coord.lon}`;
+  humidity.innerText = `Humidity:  ${weather.main.humidity}`;
+  pressure.innerText = `Pressure: ${weather.main.pressure}`;
+  speed.innerText = `Speed: ${weather.wind.speed}`;
+  status.innerText = `Status: ${weather.weather[0].description}`;
+  temperature.innerHTML = `Temperature: ${weather.main.temperature}`;
+  // city.innerText += `${weather.name}, ${weather.sys.country}`;
+  // city.innerText += `${weather.name}, ${weather.sys.country}`;
+}
 
 
 
@@ -10,19 +65,19 @@ async function getUserGeoLocation() {
 
 
   // New Promise
-  return new Promise((resolve,reject)=>{
+  return new Promise((resolve, reject) => {
 
     const geoLocation = checkGeoLocation();
 
     if (geoLocation === null) return reject("Browsers does not have support for geo location");
-  
 
-     geoLocation.getCurrentPosition(
+
+    geoLocation.getCurrentPosition(
       (position) => {
         Latitude = position.coords.latitude;
         Longitude = position.coords.longitude;
-  
-        resolve({lat:Latitude,log:Longitude});
+
+        resolve({ lat: Latitude, log: Longitude });
       },
       (err) => {
         if (err.PERMISSION_DENIED) {
@@ -49,7 +104,7 @@ function checkGeoLocation() {
   }
 }
 
-async function getWeatherInfo({lat,log}) {
+async function getWeatherInfo({ lat, log }) {
 
   if (!lat || !log) return alert("Location Access Is Required");
 
@@ -65,14 +120,14 @@ async function getWeatherInfo({lat,log}) {
 }
 
 
-function getEle(id=""){
-  if(!id) throw "element is required";
-  
+function getEle(id = "") {
+  if (!id) throw "element is required";
+
   return document.getElementById(id);
 }
 
 
-function updateWeatherReport(data){
+function updateWeatherReport(data) {
 
   const lat = data.coord.lat;
   const lon = data.coord.lon;
@@ -83,15 +138,50 @@ function updateWeatherReport(data){
   const pressure = data.main.pressure;
   const speed = data.wind.speed;
 
-  
+
+
+
+
   getEle("weather-temp").innerHTML = temp + " <sup>°C</sup>";
   getEle("weather-desc").innerHTML = description;
   getEle("location").innerHTML = location;
-  getEle("lat").innerHTML = "Latitude: "+ lat;
-  getEle("lon").innerHTML = "Longitude: "+ lon;
-  getEle("humidity").innerHTML = "Humidity: "+ humidity;
-  getEle("pressure").innerHTML = "Pressure: "+ pressure;
-  getEle("speed").innerHTML = "Speed: "+ speed;
+  getEle("lat").innerHTML = `Latitude: ${lat}<sup>°</sup>`;
+  getEle("lon").innerHTML = `Longitude:  ${lon}<sup>°</sup>`;
+  getEle("humidity").innerHTML = `Humidity ${humidity} g.m<sup>-3</sup>`;
+  getEle("pressure").innerHTML = `Pressure: ${pressure} Pa`;
+  getEle("speed").innerHTML = `Speed:  ${speed} mph`;
+  getEle("status").innerHTML = `Status: ${description}`;
+  getEle("temperature").innerHTML = `Temperature: ${temp}<sup>°</sup>C`;
+
+
+}
+
+function updateSearchReport(data) {
+
+  const lat = data.coord.lat;
+  const lon = data.coord.lon;
+  const temp = data.main.temp;
+  const description = data.weather[0].description;
+  const location = data.name + ", " + data.sys.country;
+  const humidity = data.main.humidity;
+  const pressure = data.main.pressure;
+  const speed = data.wind.speed;
+
+
+
+
+
+  document.querySelector(".temperature").innerHTML += temp + " <sup>°C</sup>";
+  getEle("weather-desc").innerHTML = description;
+  // getEle("location").innerHTML = location;
+  getEle("lat").innerHTML = `Latitude: ${lat}<sup>°</sup>`;
+  getEle("lon").innerHTML = `Longitude:  ${lon}<sup>°</sup>`;
+  getEle("humidity").innerHTML = `Humidity ${humidity} g.m<sup>-3</sup>`;
+  getEle("pressure").innerHTML = `Pressure: ${pressure} Pa`;
+  getEle("speed").innerHTML = `Speed:  ${speed} mph`;
+  getEle("status").innerHTML = `Status: ${description}`;
+  getEle("temperature").innerHTML = `Temperature: ${temp}<sup>°</sup>C`;
+  getEle("location").innerHTML = `${location}`
 
 
 }
